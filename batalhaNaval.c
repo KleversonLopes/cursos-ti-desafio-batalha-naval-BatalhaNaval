8e6dd8e0edf240a3fa0ba42ec4a90aaab48e2c76
Kleverson C. Lopes <59924230+KleversonLopes@users.noreply.github.com>
2025-08-30 12:05:19 -0300

    Update batalhaNaval.c
    feat: implementação das rotinas com habilidades de ataque, não permitindo extrapolar os limites de área do tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -43,6 +43,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 void LimpaTela() {
     // Limpar a tela
@@ -62,11 +63,14 @@ void Espera() {
     getchar();
 }
 
-int tabuleiro[10][10];
+#define LINHA 10
+#define COLUNA 10
+
+int tabuleiro[LINHA][COLUNA];
 
 void iniciaTabuleiro() {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
             tabuleiro[i][j] = 0;
         }
     }
@@ -77,8 +81,8 @@ void mostraTabuleiro() {
     /*
     * Exibe o tabuleiro com as posições dos navios
     */
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
@@ -90,6 +94,45 @@ const int
     navio2[3] = {3,3,3}, 
     navio3[3] = {3,3,3}, 
     navio4[3] = {3,3,3};
+
+//habilidades especiais
+/**
+ * centro dos ataques será sempre
+ * entre as linhas 1 e 8, e
+ * entre as colunas 2 e 7
+ */
+const int cone[3][5] = {
+    {0, 0, 1, 0, 0},
+    {0, 1, 1, 1, 0},
+    {1, 1, 1, 1, 1}
+};
+
+const int cruz[3][5] = {
+    {0, 0, 1, 0, 0},
+    {1, 1, 1, 1, 1},
+    {0, 0, 1, 0, 0}
+};
+
+const int octaedro[3][5] = {
+    {0, 0, 1, 0, 0},
+    {0, 1, 1, 1, 0},
+    {0, 0, 1, 0, 0}
+};
+
+/**
+ * Rotina para receber parâmetros de posicionamento do navio via teclado
+ * @param x ponteiro para a coordenada x
+ * @param y ponteiro para a coordenada y
+ * @param orientacao ponteiro para a orientação do navio
+ */
+void recebeParametrosNavio(int* x, int* y, char* orientacao) {
+    printf("Coordenada X : ");
+    scanf("%d", x);
+    printf("Coordenada Y : ");
+    scanf("%d", y);
+    printf("Orientação (v)ertical | (h)orizontal | diagonal (d)ireita | diagonal (e)squerda: ");
+    scanf(" %c", orientacao);
+}
 
 /**
  *  posicionamento dos navios no tabuleiro
@@ -176,14 +219,100 @@ void posicionaNavio(int x, int y, char orientacao, int* navio, char* nomeNavio){
     }
 }
 
-// rotina para receber parâmetros de posicionamento do navio via teclado
-void recebeParametros(int* x, int* y, char* orientacao) {
-    printf("Coordenada X : ");
+/**
+ * Dispara o ataque
+ * @param x coordenada x do ataque
+ * @param y coordenada y do ataque
+ * @param habilidade habilidade a ser utilizada
+ */
+void disparaAtaque(int x, int y, char habilidade) {
+    // Lógica para disparar a habilidade do navio na posição (x, y)
+    printf("Disparando habilidade do navio em (%d, %d) com habilidade %c\n", x, y, habilidade);
+    // testa se o ataque esta dentro dos limites da área do tabuleiro
+    if (verificaLimites(x, y)) {
+        if (habilidade == 'c') {
+            ataqueCone(x, y);
+        } else if (habilidade == 'o') {
+            ataqueOctaedro(x, y);
+        } else if (habilidade == 'z') {
+            ataqueCruz(x, y);
+        }
+    }
+}
+
+/**
+ * centro dos ataques será sempre
+ * entre as linhas 1 e 8, e
+ * entre as colunas 2 e 7
+ */
+int verificaLimites(int x, int y) {
+    if (x < 1 || x > (LINHA - 1) || y < 2 || y > (COLUNA - 2)) {
+        printf("\n*** Ataque fora dos limites do tabuleiro! ***\n");
+        return 0;
+    }
+    return 1;
+}
+
+void ataqueCone(int x, int y) {
+    // Lógica para ataque em cone
+    /**
+     * posiciona o centro do cone e ajusta a área de ataque
+     */
+    printf("Ataque em cone na posição (%d, %d)\n", x, y);
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 5; j++) {
+            if (cone[i-1][j-1] == 1) {
+                int ataqueX = x + (i - 2); // ajusta a linha do ataque
+                int ataqueY = y + (j - 3); // ajusta a coluna do ataque
+                tabuleiro[ataqueX][ataqueY] = 1; // marca o ataque no tabuleiro
+            }
+        }
+    }
+}
+
+void ataqueCruz(int x, int y) {
+    // Lógica para ataque em cruz
+    printf("Ataque em cruz na posição (%d, %d)\n", x, y);
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 5; j++) {
+            if (cruz[i-1][j-1] == 1) {
+                int ataqueX = x + (i - 2); // ajusta a linha do ataque
+                int ataqueY = y + (j - 3); // ajusta a coluna do ataque
+                tabuleiro[ataqueX][ataqueY] = 1; // marca o ataque no tabuleiro
+            }
+        }
+    }
+}
+
+void ataqueOctaedro(int x, int y) {
+    // Lógica para ataque em octaedro
+    printf("Ataque em octaedro na posição (%d, %d)\n", x, y);
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 5; j++) {
+            if (octaedro[i-1][j-1] == 1) {
+                int ataqueX = x + (i - 2); // ajusta a linha do ataque
+                int ataqueY = y + (j - 3); // ajusta a coluna do ataque
+                tabuleiro[ataqueX][ataqueY] = 1; // marca o ataque no tabuleiro
+            }
+        }
+    }
+}
+
+/**
+ * Recebe os parâmetros de ataque do jogador.
+ * @param x coordenada x do ataque
+ * @param y coordenada y do ataque
+ * @param habilidade habilidade a ser utilizada
+ */
+char recebeParametrosAtaque(int* x, int* y, char* habilidade) {
+    printf("Coordenada X do ataque: ");
     scanf("%d", x);
-    printf("Coordenada Y : ");
+    printf("Coordenada Y do ataque: ");
     scanf("%d", y);
-    printf("Orientação (v)ertical | (h)orizontal | diagonal (d)ireita | diagonal (e)squerda: ");
-    scanf(" %c", orientacao);
+    printf("Habilidade a ser utilizada ");
+    printf("(c)one | (o)ctaedro | cru(z) | (s)air: ");
+    scanf(" %c", habilidade);
+    return tolower(*habilidade);
 }
 
 void partida() {
@@ -200,25 +329,39 @@ void partida() {
 
     // recebe dados do navio 1 para processar
     printf("\n*** Navio 1\n");
-    recebeParametros(&x, &y, &orientacao);
+    recebeParametrosNavio(&x, &y, &orientacao);
     posicionaNavio(x, y, orientacao, navio1, "Navio 1"); // posiciona navio 1 no tabuleiro
 
     // recebe dados do navio 2 para processar
     printf("\n*** Navio 2\n");
-    recebeParametros(&x, &y, &orientacao);
+    recebeParametrosNavio(&x, &y, &orientacao);
     posicionaNavio(x, y, orientacao, navio2, "Navio 2"); // posiciona navio 2 no tabuleiro
 
     // recebe dados do navio 3 para processar
     printf("\n*** Navio 3\n");
-    recebeParametros(&x, &y, &orientacao);
+    recebeParametrosNavio(&x, &y, &orientacao);
     posicionaNavio(x, y, orientacao, navio3, "Navio 3"); // posiciona navio 3 no tabuleiro
 
     // recebe dados do navio 4 para processar
     printf("\n*** Navio 4\n");
-    recebeParametros(&x, &y, &orientacao);
+    recebeParametrosNavio(&x, &y, &orientacao);
     posicionaNavio(x, y, orientacao, navio4, "Navio 4"); // posiciona navio 4 no tabuleiro
 
     mostraTabuleiro();
+    
+    do {
+        printf("\n");
+        int x, y;
+        char habilidade;
+
+        if (recebeParametrosAtaque(&x, &y, &habilidade) == 's') {
+            // Sair da partida
+            break;
+        }
+        disparaAtaque(x, y, habilidade);
+        mostraTabuleiro();
+        Espera();
+    } while (1);
 }
 
 void main() {
